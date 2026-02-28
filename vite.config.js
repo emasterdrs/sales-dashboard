a0e6dc063e46fbe650396cab30d8c002ccafce2b
emasterdrs <emasterdrs@users.noreply.github.com>
2026-02-28 13:29:01 +0900

    Deployment: change base path to '/' for Vercel support

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/sales-dashboard/',
+  base: '/',
   build: {
     minify: false,
     cssMinify: false
