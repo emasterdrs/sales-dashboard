54406bb49e0d0d56f52ccda8c8c3cdf0efbb76d5
emasterdrs <emasterdrs@users.noreply.github.com>
2026-03-03 05:08:13 +0900

    Fix vite config base for Vercel deployment

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/sales-dashboard/',
+  base: process.env.VERCEL ? '/' : '/sales-dashboard/',
   build: {
     minify: false,
     cssMinify: false
